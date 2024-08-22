479de830b2e1a66a1b66ac40e9b82332a9f4cbde
Vinicius Bahia <vini.bahiarj@gmail.com>
2024-08-22 20:43:12 -0300

    adiciona arquivos sql úteis

--- utils/reset.sql
+++ utils/reset.sql
@@ -1,2 +1,5 @@
+DELETE FROM documentapp_signer;
+DELETE FROM documentapp_document;
+DELETE FROM documentapp_company;
 INSERT INTO documentapp_company (name, api_token, created_at, last_updated_at)
 VALUES ('ZapSign', '<API_TOKEN>', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
