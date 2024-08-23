eb400d14238c98565e933bb11c516acdab2f43ad
Vinicius Bahia <vini.bahiarj@gmail.com>
2024-08-22 23:19:32 -0300

    exibe mensagem para diferentes estados da obtenção de documentos

--- frontend/src/services/get-docs.service.ts
+++ frontend/src/services/get-docs.service.ts
@@ -6,19 +6,18 @@ import {BehaviorSubject, Observable} from 'rxjs';
 export class GetDocsService {
     private readonly _apiUrl = 'http://localhost:8000/documentapp/read'
     private readonly _http = inject(HttpClient)
-    private readonly _docs$ = new BehaviorSubject<any[]>([])
+    private readonly _docs$ = new BehaviorSubject<GetDocsState>([])
     readonly docs$ = this._docs$.asObservable()
 
-    execute(): Observable<any[]> {
+    execute(): Observable<GetDocsState> {
+        this._docs$.next('loading')
         const subscription = this._http.get<any[]>(this._apiUrl).subscribe({
-            next: (response) => {
-                this._docs$.next(response);
-            },
-            error: (error) => {
-                this._docs$.next([])
-            },
+            next: response => this._docs$.next(response),
+            error: () => this._docs$.next('error'),
             complete: () => subscription.unsubscribe()
         })
         return this.docs$
     }
 }
+
+type GetDocsState = any[] | 'loading' | 'error'
